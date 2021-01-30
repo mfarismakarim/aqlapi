25a722d5a68a4541906e54014bc760b77af2b5d9
Averlyn <mfmakarim@gmail.com>
2021-01-30 09:04:02 +0700

    Adding Volunteer CRUD

--- app/Http/Controllers/VolunteerController.php
+++ app/Http/Controllers/VolunteerController.php
@@ -149,4 +149,51 @@ class VolunteerController extends Controller
             return response()->json(["success" => false, "message" => $err]);
         }
     }
+
+    public function editVolunteerAuth(Request $request)
+    {
+        if(!$model = Volunteer::find($request->get('id'))){
+            return response()->json('Data Tidak Ditemukan');
+        }
+        if($request->get('NIK') !== null) $model->NIK = $request->get('NIK');
+        if($request->get('namaLengkap') !== null) $model->namaLengkap = $request->get('namaLengkap');
+        if($request->get('namaPanggilan') !== null) $model->namaPanggilan = $request->get('namaPanggilan');
+        if($request->get('tempatLahir') !== null) $model->tempatLahir = $request->get('tempatLahir');
+        if($request->get('tanggalLahir') !== null) $model->tanggalLahir = $request->get('tanggalLahir');
+        if($request->get('umur') !== null) $model->umur = $request->get('umur');
+        if($request->get('status') !== null) $model->status = $request->get('status');
+        if($request->get('jumlahSaudara') !== null) $model->jumlahSaudara = $request->get('jumlahSaudara');
+        if($request->get('jenisKelamin') !== null) $model->jenisKelamin = $request->get('jenisKelamin');
+        if($request->get('anakKe') !== null) $model->anakKe = $request->get('anakKe');
+        if($request->get('alamat') !== null) $model->alamat = $request->get('alamat');
+        if($request->get('facebook') !== null) $model->facebook = $request->get('facebook');
+        if($request->get('instagram') !== null) $model->instagram = $request->get('instagram');
+        if($request->get('twitter') !== null) $model->twitter = $request->get('twitter');
+        if($request->get('noHp') !== null) $model->noHp = $request->get('noHp');
+        if($request->get('whatsapp') !== null) $model->whatsapp = $request->get('whatsapp');
+        if($request->get('email') !== null) $model->email = $request->get('email');
+        if($request->get('tempatMengaji') !== null) $model->tempatMengaji = $request->get('tempatMengaji');
+        if($request->get('motivasi') !== null) $model->motivasi = $request->get('motivasi');
+        if($request->get('harapan') !== null) $model->harapan = $request->get('harapan');
+        if($request->get('komitmen') !== null) $model->komitmen = $request->get('komitmen');
+        try{
+            $model->save();
+            return response()->json(["success" => true, "message" => "Data Berhasil Dirubah"]);
+        } catch(Exception $err){
+            return response()->json(["success" => false, "message" => $err]);
+        }
+    }
+
+    public function deleteVolunteerAuth(Request $request)
+    {
+        $id = $request->get('id', null);
+        $model = Volunteer::find($id);
+        if($model === null) return response()->json(["success" => false, "message" => "Data Tidak Ditemukan"]);
+        try{
+            $model->delete();
+            return response()->json(["success" => true, "message" => "Data Berhasil Dihapus"]);
+        } catch(Exception $err){
+            return response()->json(["success" => false, "message" => $err]);
+        }
+    }
 }
